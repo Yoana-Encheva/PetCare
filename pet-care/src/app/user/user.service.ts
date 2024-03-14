9d755e89b65dcae1b562c8bb0007bc2400025b9f
Yoana-Encheva <je.encheva@gmail.com>
2024-03-14 17:31:07 +0200

    Add dummy login and logout

--- pet-care/src/app/user/user.service.ts
+++ pet-care/src/app/user/user.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { User } from '../types/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  user: User | undefined;
+  USER_KEY = '[user]';
 
-  constructor() { }
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
+  constructor() {
+    try {
+      const userData = localStorage.getItem(this.USER_KEY) || '';
+      this.user = JSON.parse(userData);
+    } catch (err) {
+      this.user = undefined;
+    }
+  }
+
+  login() {
+    this.user = {
+      name: 'John',
+      email: 'test@abv.bg',
+      password: '123456',
+    };
+
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY);
+  }
 }
