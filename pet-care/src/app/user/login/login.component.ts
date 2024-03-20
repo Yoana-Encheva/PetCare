9c76e7eed8a6d9db9ac8f433d9052278a0312576
Yoana-Encheva <je.encheva@gmail.com>
2024-03-20 18:08:50 +0200

    Add some design enhancements

--- pet-care/src/app/user/login/login.component.ts
+++ pet-care/src/app/user/login/login.component.ts
@@ -9,7 +9,22 @@ import { UserService } from '../user.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  show: boolean = false;
+
   constructor(private userService: UserService, private router: Router) {}
+
+  submit() {
+    console.log('user name is ' + this.username);
+    this.clear();
+  }
+
+  clear() {
+    this.username = '';
+    this.password = '';
+    this.show = true;
+  }
 
   login(event: Event, email: string, password: string) {
     event.preventDefault();
--- pet-care/src/app/user/user.module.ts
+++ pet-care/src/app/user/user.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import { UserRoutingModule } from './user-routing.module';
 
 import { LoginComponent } from './login/login.component';
@@ -8,6 +13,13 @@ import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [LoginComponent, ProfileComponent, RegisterComponent],
-  imports: [CommonModule, UserRoutingModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    UserRoutingModule,
+    FormsModule,
+  ],
 })
 export class UserModule {}
