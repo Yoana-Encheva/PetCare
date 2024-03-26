be78254ac713a53708232908f0550af71de08216
Yoana-Encheva <je.encheva@gmail.com>
2024-03-26 15:10:44 +0200

    Add http interceptor

--- pet-care/src/app/app.module.ts
+++ pet-care/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { SharedModule } from './shared/shared.module';
 import { MaterialModule } from './material/material.module';
 import { UserModule } from './user/user.module';
 
+import { appInterceptorProvider } from './app-interceptor.interceptor';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MainComponent } from './main/main.component';
@@ -28,7 +30,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     UserModule,
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [appInterceptorProvider],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
