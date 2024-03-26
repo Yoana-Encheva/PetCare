5a72d9d2e659299116d749b8e9ef3c1988063ffb
Yoana-Encheva <je.encheva@gmail.com>
2024-03-26 15:13:40 +0200

    Clean up

--- pet-care/src/app/user/login/login.component.ts
+++ pet-care/src/app/user/login/login.component.ts
@@ -10,21 +10,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  username: string = '';
-  password: string = '';
-  show: boolean = false;
-
   constructor(private userService: UserService, private router: Router) {}
-
-  submit() {
-    this.clear();
-  }
-
-  clear() {
-    this.username = '';
-    this.password = '';
-    this.show = true;
-  }
 
   login(form: NgForm) {
     if (form.invalid) {
