d93739829e5f1906d5b57cf13ae748fe49f943d3
Yoana-Encheva <je.encheva@gmail.com>
2024-03-30 12:06:24 +0200

    Create display date pipe

---
+++ pet-care/src/app/shared/pipes/display-date.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { formatDate } from '@angular/common';
+@Pipe({
+  name: 'displayDate',
+})
+export class DisplayDatePipe implements PipeTransform {
+  transform(date: number, ...args: unknown[]): unknown {
+    return formatDate(date, 'dd.MM.yy, h:mm a', 'en-US');
+  }
+}
--- pet-care/src/app/shared/shared.module.ts
+++ pet-care/src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { EmailDirective } from './validators/email.directive';
+import { DisplayDatePipe } from './pipes/display-date.pipe';
 import { SlicePipe } from './pipes/slice.pipe';
 
 @NgModule({
-  declarations: [LoaderComponent, EmailDirective, SlicePipe],
+  declarations: [LoaderComponent, EmailDirective, DisplayDatePipe, SlicePipe],
   imports: [CommonModule],
-  exports: [LoaderComponent, EmailDirective, SlicePipe],
+  exports: [LoaderComponent, EmailDirective, DisplayDatePipe, SlicePipe],
 })
 export class SharedModule {}
