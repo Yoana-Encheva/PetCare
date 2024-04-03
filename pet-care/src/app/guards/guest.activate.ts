7fa84e5d6c3bab1f36cdc4c2f5503464e4a375cd
Yoana-Encheva <je.encheva@gmail.com>
2024-04-03 19:56:19 +0300

    Protect login and register from already logged users

---
+++ pet-care/src/app/guards/guest.activate.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class GuestActivate implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    const isLoggedIn = Boolean(localStorage.getItem('[user-id]') || null);
+
+    if (isLoggedIn) {
+      this.router.navigate(['/home']);
+      return false;
+    }
+
+    return true;
+  }
+}
--- pet-care/src/app/user/user-routing.module.ts
+++ pet-care/src/app/user/user-routing.module.ts
@@ -4,10 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AuthActivate } from '../guards/auth.activate';
+import { GuestActivate } from '../guards/guest.activate';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent, canActivate: [GuestActivate] },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [GuestActivate],
+  },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthActivate] },
 ];
 
